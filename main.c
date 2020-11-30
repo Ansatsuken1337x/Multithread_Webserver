2b380944361ec394a4d3597b56b2e6ebdc4b349a
Roner Rodrigues <ronerhenry@gmail.com>
2020-11-30 11:19:00 -0300

    2a versao concluida

--- main.c
+++ main.c
@@ -1,215 +1,298 @@
-// includes 
-#include <unistd.h>
+// INCLUDES 
 #include <stdio.h>
-#include <sys/socket.h>
 #include <stdlib.h>
-#include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
 #include <pthread.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h> 
 
-// defines 
+// DEFINES 
 #define MAX_CONNECTIONS 100
-#define EOL "\r\n"
-#define EOL_SIZE 2
 #define PATH_MAX 4096 
 
-// global 
-char ROOT[123123];
-char data_to_send[4096];
+// GLOBAL SEC. 
+char ROOT[PATH_MAX];
 int BUFFER_SIZE = 4096; // 4Kb = default page table size
-int PORT = 666;
+int PORT = 31337;
 
 char *responses[] = 
 {
-	"HTTP/1.1 200 OK\n\n",
-	"HTTP/1.0 404 Not Found\n",
+	"HTTP/1.1 200 OK\n",
 	"HTTP/1.0 400 Bad Request\n",
+	"HTTP/1.0 403 Forbidden\n",
+	"HTTP/1.0 404 Not Found\n",
 };	
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-
-// typedef struct {
- // char *ext;
- // char *mediatype;
-// } extn;
-
-//Possible media types
-// extn extensions[] ={
- // {"gif", "image/gif" },
- // {"txt", "text/plain" },
- // {"jpg", "image/jpg" },
- // {"jpeg","image/jpeg"},
- // {"png", "image/png" },
- // {"ico", "image/ico" },
- // {"zip", "image/zip" },
- // {"gz",  "image/gz"  },
- // {"tar", "image/tar" },
- // {"htm", "text/html" },
- // {"html","text/html" },
- // {"php", "text/html" },
- // {"pdf","application/pdf"},
- // {"zip","application/octet-stream"},
- // {"rar","application/octet-stream"},
- // {0,0} };
-
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
-int handle_request(char *mesg, int socked_fd)
-{
-	char *reqline[3], path[PATH_MAX];
-	int bytes_read, bytes_write, temp, fd;
 
-	reqline[0] = malloc(sizeof(strlen(mesg)));
+void getClientAddr(int sock_fd)
+{
+	char clientip[200];
+	struct sockaddr_in addr;
+	socklen_t addr_size = sizeof(struct sockaddr_in);
 	
-	// parser the mesg received
-	printf("%s \n",mesg);
+
+	int res = getpeername(sock_fd,(struct sockaddr *)&addr, &addr_size);
+	strcpy(clientip, inet_ntoa(addr.sin_addr));
+
+	printf("Client connected. [IP addr: %s]\n",clientip);
+}
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+void handle_request(char *client_request, int sock_fd)
+{
+	char *reqline[3], path[PATH_MAX], data_to_send[BUFFER_SIZE];
+	int bytes_read, bytes_write, temp, fd, c;
+	size_t content_length;
+	FILE *fp;
+
+	reqline[0] = malloc(sizeof(strlen(client_request)));
+	reqline[1] = malloc(BUFFER_SIZE);
+	reqline[2] = malloc(BUFFER_SIZE);
+	
+
+	// parser the request received from client
+	printf("\033[1;92mPARSING REQUEST...\033[0m\n");
+	// printf("%s",client_request);
+
 	// reqline[0] = http method
-	// reqline[1] = protocol version
-	// reqline[2] = file required
-	reqline[0]	= strtok(mesg," ");
-	reqline[1] = strtok(NULL," ");
-	reqline[2] = strtok(NULL,EOL);
+	// reqline[1] = file required
+	// reqline[2] = protocol version
+	reqline[0]	= strtok(client_request," ");
+	reqline[1] 	= strtok(NULL," ");
+	reqline[2] 	= strtok(NULL,"\r\n");
 	
+
+	// ONLY accepts HTTP/1.0 and HTTP/1.1 request headers from client
+	// Persistent connection is an opt-in feature, anyway 
 	if (strncmp(reqline[2], "HTTP/1.0", 8)!=0 && strncmp(reqline[2], "HTTP/1.1", 8)!=0 )
 	{
-		write(socked_fd, responses[2], strlen(responses[2]));
-		return -1;
+
+		send(sock_fd, responses[2], strlen(responses[2]), 0);
+		return;
 	}
+	
 	
 	printf("reqline[0]	= %s \n",reqline[0]);
 	printf("reqline[1]	= %s \n",reqline[1]);
 	printf("reqline[2]	= %s \n",reqline[2]);
 	
+
+	/*	****************************** HANDLE HTTP METHODS ****************************** */
 	//	GET METHOD
 	if(strcmp(reqline[0],"GET") == 0)
 	{		
-
-
 		if ( strncmp(reqline[1], "/\0", 2)==0 )
-			//Because if no file is specified, index.html will be opened by default   
-			// strcpy(reqline[1],"/index.html");
-			strcpy(reqline[1],"/cat.jpg");
-	
-		printf("ROOT= %s \n",ROOT);
+			// If no file is specified, index.html will be opened by default   
+			// strcpy(reqline[1],"/cat.jpg");
+			strcpy(reqline[1],"/index.html");
 
-		
+	
+		// printf("ROOT = %s \n",ROOT);	
 		strcpy(path, ROOT);
 		strcpy(&path[strlen(ROOT)], reqline[1]);
-		printf("file: %s\n", path);
+		printf("File Required = %s\n", path);
 	
 		
-		if ( (fd=open(path, 0)) != -1 )	//FILE FOUND
+		bytes_read = 0;
+		bytes_write = 0;
+		temp = 0;
+		if (fp = fopen(path, "r" ))	//FILE FOUND
 		{
-			send(socked_fd, responses[0], strlen(responses[0]), 0);
+			fd = fileno(fp);
 
-			//	BEGIN OF CRITICAL SECTION
-			pthread_mutex_lock(&lock); 
+			c = fgetc(fp);
+			while(c != EOF)
+			{
+				// data_to_send[bytes_read] = c;
+				bytes_read++;
+				c = fgetc(fp);
+			}
+			// data_to_send[bytes_read] = '\0';
+			rewind(fp);
+
+			char str[4];
+			sprintf(str, "%d", bytes_read);
+			send(sock_fd, responses[0], strlen(responses[0]), 0);
+			send(sock_fd, "Connection: keep-alive\n", strlen("Connection: keep-alive\n"), 0);
+			send(sock_fd, "Content-Length: ", strlen("Content-Length: "), 0);
+			send(sock_fd, str, strlen(str), 0);
+			send(sock_fd, "\n", 1, 0);
+			// send(sock_fd, "Keep-Alive: max=3\n", strlen("Keep-Alive: max=3\n"), 0);
+			send(sock_fd, "Keep-Alive: timeout=3\n\n", strlen("Keep-Alive: timeout=3\n\n"), 0);
+			// send(sock_fd, "\n\n", 2, 0);
+
 			
+			bytes_read = 0;
 			while ( (temp = read(fd, data_to_send, BUFFER_SIZE)) > 0 )
 			{
+				
 				bytes_read += temp;
-				bytes_write = write(socked_fd, data_to_send, temp);
-				// printf("data_to_send = %s\n",data_to_send);
+				bytes_write += write(sock_fd, data_to_send, temp);
 			}
-			printf("\nbytes_read = %d", bytes_read);
-			printf("\nbytes_write = %d\n", bytes_write);
+			// bytes_write += write(sock_fd, data_to_send, bytes_read);
+			fclose(fp);	
 			
-			pthread_mutex_unlock(&lock);
-			//	END OF CRITICAL SECTION
-		}
 		
+			printf("bytes_read = %d\n", bytes_read);
+			printf("bytes_write = %d\n\n", bytes_write);
+		}
 		else
 		{
-			write(socked_fd, responses[1], strlen(responses[1])); //FILE NOT FOUND
-			return 400;
+			write(sock_fd, responses[1], strlen(responses[1])); //FILE NOT FOUND
+			printf("File not found.\n");
+			return;
 		}
 	}
 	
-	//	POST METHOD
-	else if(strcmp(reqline[0],"POST") == 0)
-	{
-		printf("reqline[0]	= %s \n",reqline[0]);
-		printf("reqline[1]	= %s \n",reqline[1]);
-		printf("reqline[2]	= %s \n",reqline[2]);
-	}
-	
-	shutdown(socked_fd, SHUT_RDWR);
-	close(socked_fd);
-			
-	return 200;
+
+	return;
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////
+
 
 void * respond(void *arg)
 {
-	// optimal initial BUFFER_SIZE = ???
-	char mesg[BUFFER_SIZE];
-	int bytes_read, socked_fd, thread_ret;
+	int i, j, bytes_read, crlf_count, singleByte, sock_fd, total_requests;
+	char clientRequest[BUFFER_SIZE], singleChar[1];
+	clock_t t;
 	
-	memset((void*)mesg, (int)'\0', BUFFER_SIZE);
+	// get socket FD 
+	sock_fd = *((int *)arg);
+	printf("#[sock_fd]: %d\n", sock_fd);
 	
-	// get socket fd
-	// ptr_thread_arg targ = (ptr_thread_arg)arg;
-	// socked_fd = (ptr_thread_arg)targ->socket;
-	socked_fd = *((int *)arg);
+	// initialize consumer variables
+	memset((void*)clientRequest, (int)'\0', BUFFER_SIZE);
+	singleChar[0] = '\0';
+	bytes_read = 0;
+	crlf_count = 0;
+	total_requests = 0;
+	i = 0, j = 0;
+
+	// read data from client socket until find 2x CRLF's, or the timeout param value has achieved their limit
+	// default timeout set to 3 sec's
+	while (1)
+	{	
+		
+		if(j == 0)
+			printf("\n\033[1;35mRECEIVING HEADERS...\033[0m\n");
+		
+		// start timeout mark
+		// t = clock();
 	
-    bytes_read = read(socked_fd, mesg, BUFFER_SIZE);
-	if (bytes_read < 0) // receive error
-	{
-		fprintf(stderr,"Error receiving data from client.\n");
-		// return -1;
-		exit;
+		singleByte = recv(sock_fd, singleChar, 1, 0);
+		if (singleByte < 0) // receive error
+		{
+			fprintf(stderr,"Error receiving data from client.\n");
+			break;
+		}
+		else if (singleByte == 0) // socket closes
+		{
+			fprintf(stderr,"Client disconnected unexpectedly.\n");
+			break;
+		}
+		bytes_read++;
+
+
+		// clients header already received here
+		// verifys '\r\n\r\n' sequence
+		// if(bytes_read == )
+		if(singleChar[0] == '\n' && clientRequest[i - 2] == '\n')
+		{						
+		
+			clientRequest[i] = singleChar[0];
+			printf("%c",clientRequest[i]);
+			// printf("#[bytes_read] CLIENT REQUEST HEADER = %d\n", bytes_read);
+
+
+			// handle request type properly
+			handle_request(clientRequest, sock_fd);
+			
+			// prepare to a new request header
+			memset((void*)clientRequest, (int)'\0', BUFFER_SIZE);
+			bytes_read = 0;
+			crlf_count = 0;
+			i = 0;
+			j = 0;
+			continue;		
+		}
+
+		else if(singleChar[0] == '\n' && crlf_count == 0)
+		{
+			clientRequest[i] = singleChar[0];
+			printf("%c",clientRequest[i]);
+			crlf_count = 1;
+			j = 1;
+			i++;
+			continue;	
+		}
+
+		else
+		{
+			clientRequest[i] = singleChar[0];
+			printf("%c",clientRequest[i]);
+			j = 1;
+			i++;
+			continue;	
+		}
 	}
-	else if (bytes_read == 0) // socket closes
-	{
-		fprintf(stderr,"Client disconnected unexpectedly.\n");
-        // return -1;
-		exit;
-	}
-	// client header received here
-	
-	// handle request type properly
-	thread_ret = handle_request(mesg, socked_fd);
-	
-	// return thread_ret;
+
+
+	shutdown(sock_fd, SHUT_RDWR);
+	close(sock_fd);
+	pthread_exit(&sock_fd);
+	return NULL;
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
     pthread_t threads[MAX_CONNECTIONS];
-	void * retvals[MAX_CONNECTIONS] = {NULL};
-    struct sockaddr_in address;
-    int server_fd, new_socket;
-    int addrlen = sizeof(address);
-    int i = 0, j = 0;
+    struct sockaddr_in clientAddr;
+    socklen_t addrlen = sizeof(struct sockaddr_in);
+    int server_fd, sock_fd;
+    int i = 0;
+	// int *ptr[MAX_CONNECTIONS];
+
 	
 	// If port number is not specified, set default http port
-	// PORT = (argv[1] != ' ') ? atoi(argv[1]) : 80;
-	PORT = 80;
-	
+	PORT = ((argv[1] != (char *)' ') && (argc >= 2)) ? atoi(argv[1]) : PORT;
+
 	// ROOT to serve files
-	// ROOT = '/home/roner/Desktop/redes2020.1/Multithread_Webserver-master/';
-	strcpy(ROOT,"/home/roner/Desktop/redes2020.1/Multithread_Webserver-master");
+	strcpy(ROOT,getenv("PWD"));
     
     // Creating socket file descriptor
+    // if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("socket failed");
         exit(1);
     }
 	
-	address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+	clientAddr.sin_family = AF_INET;
+    clientAddr.sin_addr.s_addr = INADDR_ANY;
+    clientAddr.sin_port = htons(PORT);
 
-	// Attach socket to the address:port specified 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+	// Attach socket to the clientAddr:port specified 
+    if (bind(server_fd, (struct sockaddr *)&clientAddr, sizeof(clientAddr)) < 0)
     {
         perror("bind failed");
         exit(1);
     }
 	
+	// char content_length[BUFFER_SIZE];
+	// strcpy(content_length,"BANANA = ");
+	// int size = strlen(content_length);
+	// printf("\nPLANO = %s\n",content_length);
+	// // printf("\nPLANO = %s\n",strcat("Content-Length: ",(char *)itoa(strlen("adasdas"))));
+	// exit(0);
+
 	// run the server
     while (1)
     {
@@ -219,41 +302,58 @@ int main(int argc, char **argv)
             exit(1);
         }
 		
-        printf("\033[92mAceitando a requisição # %d \033[0m\n",j);
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        printf("\033[1;37mLISTENING CONNECTION... # %d \033[0m\n",i);
+        if ((sock_fd = accept(server_fd, (struct sockaddr *)&clientAddr, (socklen_t *)&addrlen)) < 0)
         {
-            perror("Cannot accept connection");
-            exit(1);
+            
+			perror("Cannot accept connection");
+			exit(1);
         }
-        printf("Conexão estabelecida. \n");
-		// increments # total conns
-		j++;
-		i++;
+		// else
+		// {
+		// 	int enable = 1;
+		// 	if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+    	// 		perror("setsockopt(SO_REUSEADDR) failed");
+		// }
 		
-		// Find a thread that's not busy
-	    	// VOU USAR ISSO AQUI POSTERIORMENTE !!
-		while(retvals[i % MAX_CONNECTIONS] != NULL)
-		{
-			i++;
-		}
 	
-        if (pthread_create(&threads[i], NULL, respond, &new_socket) != 0)
+		// Get and prints client address 
+        getClientAddr(sock_fd);
+
+		// SEQUENCIAL 
+		// respond(&sock_fd);
+
+		// MULTITHREAD
+		// Create thread and handle request
+        if (pthread_create(&threads[i % MAX_CONNECTIONS], NULL, &respond, &sock_fd) != 0)
 		{
-			fprintf(stderr, "error: Cannot create thread # %d\n", j);
+			fprintf(stderr, "error: Cannot create thread # %d\n", i);
 			break;	
 		}
+
+		//	BEGIN OF CRITICAL SECTION
+		// pthread_mutex_lock(&lock); 
+			
+		// pthread_mutex_unlock(&lock);
+		//	END OF CRITICAL SECTION
 	
-		if (pthread_join(threads[i], NULL) != 0)
+		// Wait thread finish their job
+		if (pthread_join(threads[i % MAX_CONNECTIONS], NULL) != 0)
         {
-			fprintf(stderr, "error: Cannot join thread # %d\n", j);
+			fprintf(stderr, "error: Cannot join thread # %d\n", i);
 			break;
         }	
 		else 
-		{
-			// printf("Thread returns = %d @%p\n",retvals[i],retvals[i]);
-			retvals[i] = NULL;
+		{	
+			
+			printf("\033[1;31mTHREAD [# %d] HAS SUCCESSFULLY FINISHED THEIR JOB. \033[0m \n\n",i);
 		}
+
+		// increments # total conns
+		i++;
     }
+
+
 
     return 0;
 }
